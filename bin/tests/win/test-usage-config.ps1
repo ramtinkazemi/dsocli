7ca56c49b3e788f3d63f632912ec0922899f50f0
Ramtin Kazemi <ramtin.kazemi@sourcedgroup.com>
2022-11-23 13:39:00 +1100

    v1.0.194.dev0

--- bin/tests/win/test-usage-config.ps1
+++ bin/tests/win/test-usage-config.ps1
@@ -27,39 +27,39 @@ if(!(Test-Path tests\output\))
 ##################################
 
 Write-Output "`ndso config init -v5 --global-scope`n"
-Invoke-Call -ScriptBlock {dso config init -v5 --global-scope} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config init -v5 --global-scope} -ErrorAction Stop
 
 Write-Output "`ndso config list -v5 --global-scope`n"
-Invoke-Call -ScriptBlock {dso config list -v5 --global-scope} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config list -v5 --global-scope} -ErrorAction Stop
 
 Write-Output "`ndso config list -v5 -w `"${working_dir}`" --local`n"
-Invoke-Call -ScriptBlock {dso config list -v5 -w "${working_dir}"} --local -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config list -v5 -w "${working_dir}"} --local -ErrorAction Stop
 
 Write-Output "`ndso config add -v5 test.global-config some-value --global-scope`n"
-Invoke-Call -ScriptBlock {dso config add -v5 test.global-config some-value --global-scope} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config add -v5 test.global-config some-value --global-scope} -ErrorAction Stop
 
 Write-Output "`ndso config get -v5 test.global-config some-value --global-scope`n"
-Invoke-Call -ScriptBlock {dso config get -v5  test.global-config --global-scope} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config get -v5  test.global-config --global-scope} -ErrorAction Stop
 
 Write-Output "`ndso config init -v5 -w `"${working_dir}`"`n"
-Invoke-Call -ScriptBlock {dso config init -v5 -w "${working_dir}"} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config init -v5 -w "${working_dir}"} -ErrorAction Stop
 
 Write-Output "`ndso config add -v5 -w `"${working_dir}`" test.local-config some-value`n"
-Invoke-Call -ScriptBlock {dso config add -v5 -w "${working_dir}" test.local-config some-value} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config add -v5 -w "${working_dir}" test.local-config some-value} -ErrorAction Stop
 
 Write-Output "`ndso config get -v5 -w `"${working_dir}`" test.local-config`n"
-Invoke-Call -ScriptBlock {dso config get -v5 -w "${working_dir}" test.local-config} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config get -v5 -w "${working_dir}" test.local-config} -ErrorAction Stop
 
 Write-Output "`ndso config delete -v5 test.global-config --global-scope`n"
-Invoke-Call -ScriptBlock {dso config delete -v5 test.global-config --global-scope} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config delete -v5 test.global-config --global-scope} -ErrorAction Stop
 
 Write-Output "`ndso config delete -v5 -w `"${working_dir}`" test.local-config`n"
-Invoke-Call -ScriptBlock {dso config delete -v5 -w "${working_dir}" test.local-config} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config delete -v5 -w "${working_dir}" test.local-config} -ErrorAction Stop
 
 Write-Output "`ndso config add -v5 -w `"${working_dir}`" namespace ${namespace}`n"
-Invoke-Call -ScriptBlock {dso config add -v5 -w "${working_dir}" namespace ${namespace}} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config add -v5 -w "${working_dir}" namespace ${namespace}} -ErrorAction Stop
 
 Write-Output "`ndso config add -v5 -w `"${working_dir}`" application ${application}`n"
-Invoke-Call -ScriptBlock {dso config add -v5 -w "${working_dir}" application ${application}} -ErrorAction Stop > $null
+Invoke-Call -ScriptBlock {dso config add -v5 -w "${working_dir}" application ${application}} -ErrorAction Stop
 
 
